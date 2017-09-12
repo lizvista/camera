34fcd94d28598181098c65f8c6a57eb0c3ff8a32
Jon Rauschenberger <jonrauschenberger@Jons-MacBook-Pro.local>
2017-09-12 15:25:57 -0500

    camera permissions included

--- camera/index.ios.js
+++ camera/index.ios.js
@@ -8,46 +8,55 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  Dimensions,
+  TouchableHighlight,
   Text,
   View
 } from 'react-native';
+import Camera from 'react-native-camera';
 
 export default class camera extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    takePicture() {
+        const options = {};
+        this.camera.capture({metadata: options})
+        .then((data) => console.log(data))
+        .catch(err => console.log(err));
+    }
+    
+    render() {
+        return (
+          <View style={styles.container}>
+            <Camera
+                ref={(cam) => {
+                this.camera = cam;
+                }}
+                style={styles.preview}
+                aspect={Camera.constants.Aspect.fill}>
+                <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+            </Camera>
+          </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        color: '#000',
+        padding: 10,
+        margin: 40,
+    },
 });
 
 AppRegistry.registerComponent('camera', () => camera);
